4f4f03827258522876f6bb66138dbcce86e93672
patricijo <sebastian.patrici@gmail.com>
2025-01-13 14:47:06 +0100

    added state to page provider

--- src/components/Pages/Provider.tsx
+++ src/components/Pages/Provider.tsx
@@ -1,10 +1,12 @@
 'use client'
 import { Page } from '@/payload-types'
-import React, { createContext, useContext, ReactNode } from 'react'
+import React, { createContext, useContext, ReactNode, useState } from 'react'
 
 // Define the shape of the context data
 interface PagesContextType {
   pages: Page[]
+  activePage: Page | null | undefined
+  setActivePage: React.Dispatch<React.SetStateAction<Page | null | undefined>>
 }
 
 // Create the context with a default value of `undefined`
@@ -21,5 +23,11 @@ export const usePages = (): PagesContextType => {
 
 // Create the provider component
 export const PagesProvider = ({ children, pages }: { children: ReactNode; pages: Page[] }) => {
-  return <PagesContext.Provider value={{ pages }}>{children}</PagesContext.Provider>
+  const [activePage, setActivePage] = useState<Page | null | undefined>()
+
+  return (
+    <PagesContext.Provider value={{ pages, activePage, setActivePage }}>
+      {children}
+    </PagesContext.Provider>
+  )
 }
